eb6e3b374f4f721d8d0018249d84ea11bdfb370c
Trieu Dep Trai <trantrieudaika@gmail.com>
2025-06-11 10:55:48 +0700

    Intent 1

---
+++ Bai7_Intent/app/src/main/java/com/example/intent1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.intent1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnopen;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        btnopen = findViewById(R.id.btnopen);
+        btnopen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // khai báo Intent
+                Intent myintent = new Intent(MainActivity.this, ChildActivity.class);
+                startActivity(myintent);
+            }
+        });
+    }
+}
