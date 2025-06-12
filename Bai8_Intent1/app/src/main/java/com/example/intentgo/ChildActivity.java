dc27b39729b2c631b4f16aa2c2fd4c5490e365c5
Trieu Dep Trai <trantrieudaika@gmail.com>
2025-06-12 16:20:47 +0700

    Hoan thanh Intent truyen du lieu

--- Bai8_Intent1/app/src/main/java/com/example/intentgo/ChildActivity.java
+++ Bai8_Intent1/app/src/main/java/com/example/intentgo/ChildActivity.java
@@ -2,6 +2,7 @@ package com.example.intentgo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -25,5 +26,29 @@ public class ChildActivity extends AppCompatActivity {
         // nhận intent
         Intent myintent = getIntent();
         // lấy bundle
+        Bundle mybundle = myintent.getBundleExtra("mypackage");
+        int a = mybundle.getInt("soa");
+        int b = mybundle.getInt("sob");
+        // Giải pt và hiển thị kq
+        String nghiem = "";
+        if (a == 0 & b == 0)
+        {
+            nghiem = "PT vô số nghiệm";
+        }
+        else if(a == 0 & b != 0)
+        {
+            nghiem = "PT vô nghiệm";
+        }
+        else
+        {
+            nghiem = "Nghiệm PT bằng: " + (-1.0)*b/a;
+        }
+        txtkq.setText(nghiem);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
